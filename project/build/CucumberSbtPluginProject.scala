b1932bb2c5e43e12726b72790a4520777a8e1a34
Chris Turner <chris@templemore.co.uk>
2010-11-23 15:11:43 +0000

    Added publishing target

--- project/build/CucumberSbtPluginProject.scala
+++ project/build/CucumberSbtPluginProject.scala
@@ -1,5 +1,8 @@
 import sbt._
+import java.io.File
 
 class CucumberSbtPluginProject(info: ProjectInfo) extends PluginProject(info) {
 
+  override def managedStyle = ManagedStyle.Maven
+  lazy val publishTo = Resolver.file("Templemore Maven Repo", new java.io.File("target/publish"))
 }
